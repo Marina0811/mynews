b0d37e9e19e0e134981510fce8cde493e3594d0b
Marina Kato <kmario0811@gmail.com>
2021-08-09 08:59:59 +0000

    situmonsecond

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit');
 });
 
 Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
