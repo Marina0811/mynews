ec5528740e821b9d53102e120cdeef4958c51bf6
Marina Kato <kmario0811@gmail.com>
2021-08-22 08:46:23 +0000

    kadai13

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,13 @@ Route::group(['prefix' => 'admin'], function() {
     
 });
 
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create');
+     Route::post('profile/edit', 'Admin\ProfileController@edit');
+});
 
 Auth::routes();
 
