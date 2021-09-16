20082d445333a0461301078f788b0b58e467ba0e
Marina Kato <kmario0811@gmail.com>
2021-09-16 07:04:07 +0000

    kadai17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 use App\Profile;
 
+use App\History;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -54,7 +58,7 @@ class ProfileController extends Controller
         
     }
 
-    public function update()
+    public function update(Request $request)
     {
           // Validationをかける
       $this->validate($request, Profile::$rules);
@@ -66,6 +70,11 @@ class ProfileController extends Controller
 
       // 該当するデータを上書きして保存する
       $profile->fill($profile_form)->save();
+      
+      $history = new Historyprofile();
+      $history->profile_id = $profile->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
 
       return redirect('admin/profile');
         
